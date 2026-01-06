5b5224558fe7df7587e6b94ebf14d31eda1cdc9f
Mauricio Brito <mau.brito7@gmail.com>
2026-01-06 16:41:34 -0300

    alterei uma linha

--- primeiro-programa.R
+++ primeiro-programa.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(dslabs)
 data(murders)
 
+#estou vendo se o git realmente está bom
+
 murders %>%
   ggplot(aes(population, total, label = abb, color = region)) +
   geom_label()
