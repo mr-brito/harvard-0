642fb58291b38e59725aeacd3711c4f160d9ff37
Mauricio Brito <mau.brito7@gmail.com>
2026-01-06 17:06:55 -0300

    testando o novo git

--- primeiro-programa.R
+++ primeiro-programa.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(dslabs)
 data(murders)
 
-#estou vendo se o git realmente está bom
+#estou vendo se o git realmente está bom com atualização!!
 
 murders %>%
   ggplot(aes(population, total, label = abb, color = region)) +
